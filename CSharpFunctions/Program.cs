2bee4ce49e739b5de4d051e9ff1316fe52b71d5a
regi <cabael.regine@gmail.com>
2022-04-05 13:48:35 +0200

    stampaArray

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+
+//----- dichiarazioni di funzioni -----
+void StampaArray(int[] array)
+{
+
+    Console.WriteLine("[ ");
+
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length-1] + " ]");
+}
+
+int Quadrato(int numero)
+{
+    
+}
